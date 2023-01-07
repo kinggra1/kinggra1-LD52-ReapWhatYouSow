84bf4888e32d4af630eb0e999cd7f3d459a3f679
Grant <alpharock6@gmail.com>
2023-01-07 15:25:17 -0500

    Tweening Souls

--- Assets/Scripts/Crops/CropAI.cs
+++ Assets/Scripts/Crops/CropAI.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public abstract class Collectable : MonoBehaviour {
+public abstract class CropAI : MonoBehaviour {
 
     private bool triggered = false;
     protected abstract void CustomBehavior();
