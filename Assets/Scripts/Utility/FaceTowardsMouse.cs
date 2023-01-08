7834409bbb3130135bd70bbed0bc10f915922880
Grant <alpharock6@gmail.com>
2023-01-08 16:46:34 -0500

    scythe visuals rotate around mouse movement

--- Assets/Scripts/Utility/FaceTowardsMouse.cs
+++ Assets/Scripts/Utility/FaceTowardsMouse.cs
@@ -8,6 +8,8 @@ public class FaceTowardsMouse : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        float rotation = Vector2.SignedAngle(Vector3.up, mousePos - this.transform.position);
+        this.transform.rotation = Quaternion.Euler(0f, 0f, rotation);
     }
 }
